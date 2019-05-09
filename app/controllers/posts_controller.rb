4bc8b541f40786eb24f20937b47d0f26163b066e
daichi <bumpk1002@gmail.com>
2019-05-09 19:49:12 +0900

    [Finish]3-6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,32 @@ class PostsController < ApplicationController
   	  #空のモデルをビューへ渡す
   	  @post = Post.new
   end
+
+  	  #追加
+  	  def create
+  	  	  # ストロングバラメーターを使用
+  	  	  post = Post.new(post_params)
+  	  	  # DBへ保存する
+  	  	  post.save
+  	  	  # トップ画面へリダイレクト
+  	  	  redirect_to post_path(post.id)
+  	  end
+
+  	  def index
+  	  	@posts = Post.all
+
+  	  end
+
+  	  def show
+  	  	  @post = Post.find(params[:id])
+
+  	  end
+
+  	  private
+
+  	  def post_params
+  	  	  params.require(:post).permit(:title, :body)
+  	  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-
   get 'posts/new'
+
+
+
   get 'top' => 'root#top'
 
+  post 'posts' => 'posts#create'
+
+  get 'posts' => 'posts#index'
+
+  get 'posts/:id' => 'posts#show', as: 'post'
 end
